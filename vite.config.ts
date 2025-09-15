acde12a665b38cf3bd5ed41c08120b65e0851943
abxst <hainthvl@gmail.com>
2025-09-16 00:03:42 +0700

    fix domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,9 +8,8 @@ export default defineConfig({
     global: 'globalThis',
   },
   optimizeDeps: {
-    include: ['sockjs-client']
-  }
-
+    include: ['sockjs-client'],
+  },
   server: {
     proxy: {
       '/api': {
@@ -20,8 +19,4 @@ export default defineConfig({
       },
     },
   },
-})
-
-export default {
-  
-};
+});
